1951634d4cd0ee6b89c638e80478f5e2ce3bf650
cyeji <choyeji1591@gmail.com>
2023-10-22 17:17:43 +0900

    first commit

---
+++ src/main/java/com/foodexpress/superadmin/infra/exception/ExceptionAdvice.java
@@ -0,0 +1,34 @@
+package com.foodexpress.superadmin.infra.exception;
+
+
+import com.foodexpress.superadmin.modules.utils.ApiUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    private ResponseEntity<ApiUtils.ApiResult<?>> newResponse(Throwable throwable, HttpStatus status) {
+        return newResponse(throwable.getMessage(), status);
+    }
+
+    private ResponseEntity<ApiUtils.ApiResult<?>> newResponse(String message, HttpStatus status) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        return new ResponseEntity<>(fail(message, status), headers, status);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<ApiUtils.ApiResult<?>> defaultException(Exception e) {
+        log.error("defaultException", e);
+        e.printStackTrace();
+
+        return newResponse(e, HttpStatus.BAD_REQUEST);
+    }
+
+}
---
+++ src/test/java/com/foodexpress/superadmin/TestSuperAdminApplication.java
@@ -0,0 +1,23 @@
+package com.foodexpress.superadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestSuperAdminApplication {
+
+    @Bean
+    @ServiceConnection
+    MySQLContainer<?> mysqlContainer() {
+        return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(SuperAdminApplication::main).with(TestSuperAdminApplication.class).run(args);
+    }
+
+}
