7aaca14353a0475ff8dd5a9f54815ab4b1968660
cyeji <choyeji1591@gmail.com>
2023-11-19 14:52:47 +0900

    remove : 과거 코드 제거

--- src/main/java/com/foodexpress/superadmin/modules/business/repository/BusinessRepository.java
+++
@@ -1,8 +0,0 @@
-package com.foodexpress.superadmin.modules.business.repository;
-
-import com.foodexpress.superadmin.modules.business.entity.BusinessMan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BusinessRepository extends JpaRepository<BusinessMan, Long> {}
