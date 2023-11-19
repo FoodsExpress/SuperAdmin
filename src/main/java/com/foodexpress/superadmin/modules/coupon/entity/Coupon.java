4ce49a8535eb9ffe2699750beb0f4f881b02cc0e
cyeji <choyeji1591@gmail.com>
2023-11-19 19:43:49 +0900

    fix : 쿠폰 entity 수정
    - 쿠폰 유형 제거
    - 중복된 컬럼 (종료일자)
    - enum 값 @Enumerated 추가

--- src/main/java/com/foodexpress/superadmin/modules/coupon/entity/Coupon.java
+++ src/main/java/com/foodexpress/superadmin/modules/coupon/entity/Coupon.java
@@ -1,11 +1,13 @@
 package com.foodexpress.superadmin.modules.coupon.entity;
 
 import com.foodexpress.superadmin.modules.common.CommonEntity;
-import com.foodexpress.superadmin.modules.coupon.enums.CouponType;
 import com.foodexpress.superadmin.modules.coupon.enums.DetailTargetGroup;
+import com.foodexpress.superadmin.modules.coupon.enums.DiscountPolicy;
 import com.foodexpress.superadmin.modules.coupon.enums.TargetGroup;
 import com.foodexpress.superadmin.modules.event.entity.Event;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.OneToOne;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -18,17 +20,18 @@ public class Coupon extends CommonEntity {
     private String couponName;
     // 쿠폰 코드
     private String couponCode;
-    // 쿠폰 종료 일자
-    private LocalDateTime finishedAt;
-    // 쿠폰 유형
-    private CouponType couponType;
+    // 할인 정책 (퍼센트, 원)
+    @Enumerated(EnumType.STRING)
+    private DiscountPolicy discountPolicy;
     // 쿠폰 시작 날짜
     private LocalDateTime startDate;
     // 쿠폰 종료 날짜
     private LocalDateTime endDate;
     // 쿠폰 대상자
+    @Enumerated(EnumType.STRING)
     private TargetGroup couponTarget;
     // 쿠폰 상세 대상자
+    @Enumerated(EnumType.STRING)
     private DetailTargetGroup couponDetailTarget;
     // 쿠폰 제한 여부
     private boolean isLimit;
